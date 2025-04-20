ef74b7551eb2243c5c27cb4abfe0e68c2ab516d1
Hermes Neto <hermesneto53@gmail.com>
2025-04-20 03:03:32 -0300

    Update ParticipantController.java

--- src/main/java/com/hneto/combinando/controller/ParticipantController.java
+++ src/main/java/com/hneto/combinando/controller/ParticipantController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import com.hneto.combinando.model.Event;
@@ -22,6 +23,7 @@ public class ParticipantController {
     private EventService eventService;
 
     @PostMapping("/add")
+    @Transactional
     public String addParticipant(@ModelAttribute Participant participant, @RequestParam UUID eventId) {
         Event event = eventService.findEventById(eventId).orElseThrow(() -> new IllegalArgumentException("Invalid event Id:" + eventId));
         participant.setEvent(event);
